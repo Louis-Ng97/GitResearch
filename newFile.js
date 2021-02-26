9cbbf2ff833a667ce720587f1e1be2d1e0623450
Chuong Nguyen <chuong_nguyen@datahouse.com>
2021-02-26 09:55:18 +0700

    commit new file for main branch

---
+++ newFile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class newFile extends Component {
+     render() {
+          return (
+               <div>
+                    Create new file.
+               </div>
+          )
+     } 
+}
